eb336b120025c428cdb96936bf4398a6379b28c2
Azambek Jurayev <JurayevA@corp.itstep>
2023-07-05 21:56:09 +0500

    task-2

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("siu")
+class Task2():
+    def __init__(self, city, country, region, resident, city_index, phone_code):
+        self.city =city
+        self.country =country
+        self.region = region
+        self.resident = resident
+        self.city_index = city_index
+        self.phone_code = phone_code
+
+a = Task2(input("City name:"), input("Your Country :"), input("Your region"), input("count resident"), input("city index:"), input("city phone code:"))
+print("Your city:", a.city, ",", "your country:", a.country, ",", "your region:", a.region, ",", "your city residents:",
+      a.resident, ",", "your city index:", a.city_index, ",", "your city phone code:", a.phone_code)
